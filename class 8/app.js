00f8d79df6cb758e753303a4f2da1b39c290c571
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-06-04 14:09:27 +0500

    Firebase hosting & JavaScript first class

---
+++ class 8/app.js
@@ -0,0 +1,44 @@
+// alert("Hello World");
+
+// var ameen;
+
+// Data Type 
+
+// String, "My name"
+// Number,  12
+// Boolean, true /false
+// Null,   
+// Undefine 
+// Array ["ameen", "zahid"]
+// Object {name: "ameen"}
+
+// alert(ameen)
+
+var firstNumber = 10;
+var secondNumberMyName = 10;
+var thirdNumber = 10;
+
+var ab123$ = "";
+
+var myName = "123"
+
+console.log(myName)
+
+// var result = firstNumber - secondNumber * thirdNumber
+//             // 0 * 10 = 0
+//             10 - 10 * 10
+//             10 - 100
+//             -90
+
+// console.log(result, "Result")
+
+// var result = (firstNumber + secondNumber ) * thirdNumber;
+// console.log(result)
+
+
+// BODMAS = B-Brackets, O-Orders (powers/indices or roots), D-Division, M-Multiplication, A-Addition, S-Subtraction
+
+
+
+
+
