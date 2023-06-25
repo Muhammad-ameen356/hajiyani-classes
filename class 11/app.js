db7f026b694a44c86f9d1062a333c9a5b56a6e13
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-06-25 13:35:59 +0500

    Class 11

---
+++ class 11/app.js
@@ -0,0 +1,61 @@
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// var birthYear = [2002, "ameen"];
+
+// alert("Pizza \n Pizz");
+
+// console.log(birthYear);
+
+// document.write(birthYear);
+
+// var students = "ameen",
+//   secondStd = "Ahad";
+
+// console.log(students);
+// console.log(secondStd);
+
+// var students = ["ameen", "ahad", "farzana"];
+
+// var num = 0;
+
+// for(initilize; condition; i++) {
+
+// }
+
+// var i = 0;
+
+// i = i + 1;
+
+var number = 1;
+
+// if (true) {
+//   console.log("Number is less than 10");
+// }
+
+// for (number; number <= 10; number = number + 1) {
+//   //   debugger;
+//   console.log(number );
+// }
+
+// var tableValue = Number(prompt("Enter Table Value"));
+
+// console.log(tableValue);
+
+for (number; number <= 10; number = number + 1) {
+  //   debugger;
+  //   console.log(number * tableValue);
+  //   console.log(tableValue + " X " + number + " = " + number * tableValue);
+  if (number === 2) {
+    console.log("OK Number is 2");
+  } else {
+    console.log("Number in not equal to 2");
+  }
+  //   document.write();
+}
+
+// var typeof = "ameen";
+
+// Question: Write a program that finds and prints the maximum number in an array of integers.
+// Question: Write a program that calculates and prints the average of all numbers in an array.
+// Question: Write a program that counts and prints the number of even numbers in an array.
+// Question: Write a program that checks if an array contains a specific element and prints "Found" if it does or "Not found" if it doesn't.
+// Question: Write a program that multiplies each element in an array by 2 and prints the updated array.
