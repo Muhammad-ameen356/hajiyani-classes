3d6f4555448948b8850a969926e186d7a16fac1a
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-07-30 13:42:26 +0500

    Class 16

---
+++ class 16/app.js
@@ -0,0 +1,66 @@
+// var date = new Date();
+
+// date.setFullYear("2025");
+// date.setDate("23");
+// date.setMonth("2");
+// date.setMinutes(5);
+// date.setHours(1);
+// date.setSeconds(13)
+
+// console.log(date);
+
+// function sum(num1, num2) {
+//   var result = num1 + num2;
+//   var result2 = num1 + 20;
+
+//   return result;
+// }
+// function substract(num1, num2) {
+//   var result = num1 - num2;
+//   return result;
+// }
+
+// // sum(4);
+
+// // sum(14);
+
+// var functionResult = sum(2, 2) + 10;
+
+// console.log(functionResult);
+// console.log(substract(10, 5));
+
+// function findLetterIndex(letter, word) {
+//   // "Hello world".indexOf("n")
+
+//   var result = word.indexOf(letter);
+//   return result;
+// }
+
+// console.log(findLetterIndex("!", "Hello world!"))
+
+// findLetterIndex("w", "Hello world");
+
+// var ascii = "!";
+
+// console.log(ascii.charCodeAt("0"));
+
+// function findLastCharacter(word) {
+//     var result = word.charAt(word.length - 1)
+//     return result;
+// }
+
+// console.log(findLastCharacter("Hello"));
+
+// console.log(name);
+
+// var name = "ameen";
+
+// function myName() {
+//   console.log(name);
+//   var result = "Pass";
+// }
+
+// myName();
+// console.log(result);
+
+// Hoisting
